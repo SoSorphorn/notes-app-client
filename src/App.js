d159c82c94a8da94346681d1ecb2b5bfcaa1697d
sorphorn <so.sorphorn@gmail.com>
2019-03-17 21:00:22 +0700

    remove link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 import {Navbar,Nav, FormControl, Button, Form, NavItem,} from 'react-bootstrap';
 import './App.css';
 import Routes from './Routes';
