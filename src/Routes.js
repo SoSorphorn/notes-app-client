f9dc459ef9a6590efeacc91b2ece428d85fad787
sorphorn <so.sorphorn@gmail.com>
2019-03-24 11:12:14 +0700

    Done set up secure page

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import {Route,Switch} from 'react-router-dom';
-import AppliedRoute from './components/AppliedRoute';
+// import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import NotFound from "./containers/NotFound";
 import Login from './containers/Login';
 import Sigup from './containers/Signup';
 import NewNote from './containers/NewNote';
 import Notes from "./containers/Notes";
+import AuthenticatedRoute from "./components/AuthenticatedRoute"; 
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default ({childProps}) => 
   <Switch>
     {/* <Route path="/" exact component={Home} /> 
     <Route path="/login" exact component={Login} /> */}
 
-    <AppliedRoute path="/" exact component={Home} props={childProps}/>
-    <AppliedRoute path='/login' exact component={Login} props={childProps}/>
-    <AppliedRoute path='/signup' exact component={Sigup} props={childProps}/>
-    <AppliedRoute path='/notes/new' exact component={NewNote} props={childProps}/>
-    <AppliedRoute path="/notes/:id" exact component={Notes} props= {childProps} />
+    <AuthenticatedRoute path="/" exact component={Home} props={childProps}/>
+    <UnauthenticatedRoute path='/login' exact component={Login} props={childProps}/>
+    <UnauthenticatedRoute path='/signup' exact component={Sigup} props={childProps}/>
+    <AuthenticatedRoute path='/notes/new' exact component={NewNote} props={childProps}/>
+    <AuthenticatedRoute path="/notes/:id" exact component={Notes} props= {childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
