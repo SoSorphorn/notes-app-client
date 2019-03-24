1b7b4a813d2b2b09a931601dc42205641744bd1f
sorphorn <so.sorphorn@gmail.com>
2019-03-24 09:05:01 +0700

    finished display note and edit note

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Route,Switch} from 'react-router-dom';
+import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import NotFound from "./containers/NotFound";
 import Login from './containers/Login';
 import Sigup from './containers/Signup';
-import AppliedRoute from './components/AppliedRoute';
 import NewNote from './containers/NewNote';
+import Notes from "./containers/Notes";
 
 export default ({childProps}) => 
   <Switch>
@@ -16,6 +17,7 @@ export default ({childProps}) =>
     <AppliedRoute path='/login' exact component={Login} props={childProps}/>
     <AppliedRoute path='/signup' exact component={Sigup} props={childProps}/>
     <AppliedRoute path='/notes/new' exact component={NewNote} props={childProps}/>
+    <AppliedRoute path="/notes/:id" exact component={Notes} props= {childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
